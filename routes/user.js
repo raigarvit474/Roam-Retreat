e686f3b626ff86bbbb47152c2919bd08baea925e
Garvit Rai <raigarvit9@gmail.com>
2024-06-15 09:10:13 +0530

    Created Login Form

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express=require("express");
 const router=express.Router();
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const passport=require("passport");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -20,5 +21,17 @@ router.post("/signup",wrapAsync(async(req,res)=>{
     }
     
 }))
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+})
+
+router.post("/login",passport.authenticate("local",{
+    failureRedirect:'/login',
+    failureFlash:true,
+}),async(req,res)=>{
+    req.flash("success","Welcome back to WanderLust! You are logged in!");
+    res.redirect("/listings");
+});
 
 module.exports=router;
