daa8012aa00a6fc1358a4b802debdc8cf6e2a134
Garvit Rai <raigarvit9@gmail.com>
2024-06-15 12:50:07 +0530

    Implemented which page should be shown Post-Login

--- middleware.js
+++ middleware.js
@@ -1,7 +1,18 @@
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
+        //redirectUrl will be saved here 
+        req.session.redirectUrl=req.originalUrl;//store karali URL jispe jaane ki vajah se login karna pad rha hai 
         req.flash("error","You must be logged in to create a listing");
         return res.redirect("/login");
     }
     next();
 }
+
+module.exports.saveRedirectUrl=(req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl=req.session.redirectUrl;
+    }
+    next();
+}
+
+//res.locals mein save karvana pada because passport login ke time req.session ko reset kar deta hai therefore url access nhi kar paate but locals pe passport ka access nhi  hota hai issiliye locals mein store karva diya
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport=require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -32,12 +33,12 @@ router.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 })
 
-router.post("/login",passport.authenticate("local",{
+router.post("/login",saveRedirectUrl,passport.authenticate("local",{
     failureRedirect:'/login',
     failureFlash:true,
 }),async(req,res)=>{
     req.flash("success","Welcome back to WanderLust! You are logged in!");
-    res.redirect("/listings");
+    res.redirect(res.locals.redirectUrl);
 });
 
 router.get("/logout",(req,res,next)=>{
