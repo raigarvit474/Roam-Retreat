aa37466f606cfd66ccd5a5f351f322d7616c6582
Garvit Rai <raigarvit9@gmail.com>
2024-06-15 08:49:41 +0530

    Created User Model and SignUp Page

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ const ExpressError=require("./utils/ExpressError.js")
 // const Review=require("./models/review.js");
 const session=require("express-session");
 const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js")
 
-const listings=require("./routes/listing.js");
-const reviews=require("./routes/review.js")
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -55,12 +59,29 @@ app.get("/",(req,res)=>{
 app.use(session(sessionOptions)); //so now we have included session with this project so cookies are associated with each session
 app.use(flash());//always use flash before routes because flash will be used with the help of routes 
 
+//Passport uses sessions , so we will use passport below the sessions code
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
     console.log(res.locals.success);
     next();
-})
+});
+
+// app.get("/demouser",async(req,res)=>{
+//     let fakeUser=new User({
+//         email:"student@gmail.com",
+//         username:"delta-student"
+//     });
+//     let registeredUser=await User.register(fakeUser,"helloWorld");//register(user,password) also register method will automatically cheque if the username is unique or not
+//     res.send(registeredUser);
+// })
 
 // const validateListing=(req,res,next)=>{
 //     let {error}=listingSchema.validate(req.body);//using joi
@@ -157,7 +178,7 @@ app.use((req,res,next)=>{
 //This all has been commented because it has been transferred to listing.js file in routes folder so we have shifted our routes to listing .js
 
 
-app.use("/listings",listings);
+app.use("/listings",listingRouter);
 
 
 // //Reviews
@@ -199,7 +220,8 @@ app.use("/listings",listings);
 //     res.send("successfull testing");
 // })
 
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page not Found"));
